e0bb6d659e702eb5874f99b2b28afc1a5f15f430
ibrahimhossain01752 <ibrahimhossain01752@gmail.com>
2023-01-13 12:21:55 +0600

    banner added

---
+++ src/component/HomePage/Banner.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import './Banner.css';
+
+const Banner = () => {
+    return (
+        <div class='banner-main text-white'>
+            <div class="banner-overlay">
+                <div class="container">
+                    <div class="row">
+                       <div class="col-lg-6">
+                                <div class="banner-content text-center text-md-start">
+                                    <h6>Because every Business Partner Matters!</h6>
+                                    <h1>Fusion DMS 4.0</h1>
+                                    <h6>Connecting Everyone</h6>
+                                    <h2>Coming on 23 September</h2>
+                                    <div>
+                                        <button>hhh</button>
+                                        <button>jjj</button>
+                                    </div>
+                                     </div>
+                            </div>
+                            <div class="col-md-6">
+
+                            </div>
+                       
+                       
+                        
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    );
+};
+
+export default Banner;
